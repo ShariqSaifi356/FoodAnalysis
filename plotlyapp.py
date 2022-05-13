6c2660aae469bde3d01b27b3a8042d367a09ae89
Akash Kumar Yadav <akash.yadavbca2019@gmail.com>
2022-05-13 08:46:43 +0530

    commit

--- plotlyapp.py
+++ plotlyapp.py
@@ -6,249 +6,144 @@ app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 PLOTLY_LOGO = "https://images.plot.ly/logo/new-branding/plotly-logomark.png"
 
-
-
-search_bar = dbc.Row(
-    [
-        dbc.Col(
-            dbc.Button(
-                "Home", color="dark", className="ms-2", n_clicks=0
-            ),
-            width="auto",
-        ),
-        dbc.Col(
-            dbc.Button(
-                "Dataset", color="dark", className="ms-2", n_clicks=0
-            ),
-            width="auto",
-        ),
-        dbc.Col(
-            dbc.Button(
-                "About", color="dark", className="ms-2", n_clicks=0
-            ),
-            width="auto",
-        ),
-        dbc.Col(dbc.Input(type="search", placeholder="Search")),
-        dbc.Col(
-            dbc.Button(
-                "Search", color="primary", className="ms-2", n_clicks=0
-            ),
-            width="auto",
-        ),
-    ],
-    className="g-0 ms-auto flex-nowrap mt-3 mt-md-0",
-    align="center",
-)
-
-navbar = dbc.Navbar(
-    dbc.Container(
-        [
-            html.A(
-                dbc.Row(
-                    [
-                        dbc.Col(html.Img(src=PLOTLY_LOGO, height="30px")),
-                        dbc.Col(dbc.NavbarBrand("Food Production Dashboard", className="ms-2 dodgerblue")),
-                    ],
-                    align="center",
-                    className="g-0",
-                ),
-                href="https://plotly.com",
-                style={"textDecoration": "none"},
-            ),
-            dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
-            dbc.Collapse(
-                search_bar,
-                id="navbar-collapse",
-                is_open=False,
-                navbar=True,
-            ),
-        ]
+search_bar = dbc.Row([
+    dbc.Col(
+        dbc.Button(
+            "Home", color="dark", className="ms-2", n_clicks=0
+        ), width="auto"
     ),
-    color="light",
-    className="fixed-top",
-)
+    dbc.Col(
+        dbc.Button(
+            "Dataset", color="dark", className="ms-2", n_clicks=0
+        ), width="auto"
+    ),
+    dbc.Col(
+        dbc.Button(
+            "About", color="dark", className="ms-2", n_clicks=0
+        ), width="auto"
+    ),
+    dbc.Col(
+        dbc.Input(type="search", placeholder="Search")
+    ),
+    dbc.Col(
+        dbc.Button(
+            "Search", color="primary", className="ms-2", n_clicks=0
+        ), width="auto"
+    )
+], className="g-0 ms-auto flex-nowrap mt-3 mt-md-0", align="center" )
 
-card_img = dbc.Container(
-    [
-        dbc.Card(
-            [
+navbar = dbc.Navbar([
+    dbc.Container([
+        html.A(
+            dbc.Row([
+                dbc.Col(
+                    html.Img(
+                        src=PLOTLY_LOGO, 
+                        height="30px"
+                    )
+                ),
+                dbc.Col(
+                    dbc.NavbarBrand([
+                        "Food Production Dashboard"
+                    ], className="ms-2 dodgerblue")
+                )
+            ], align="center", className="g-0"),
+            href="https://plotly.com", style={"textDecoration": "none"}),
+        dbc.NavbarToggler(
+            id="navbar-toggler", 
+            n_clicks=0),
+        dbc.Collapse(
+            search_bar,
+            id="navbar-collapse",
+            is_open=False,
+            navbar=True)
+    ])
+], color="light", className="fixed-top")
+
+card_img = dbc.Container([
+    dbc.Card([
+        dbc.CardImg(
+            src="/static/images/ooter.jpg",
+            top=True,
+            style={"opacity": 0.3},
+        ),
+        dbc.CardImgOverlay(
+            dbc.CardBody([
+                html.H4("Feed All", className="title"),
+                html.P([
+                    "A Collective dashboard for food production"
+                ], className="card-text1"),
+                html.P([
+                    "over the years"
+                ], className="card-text1"),
+            ])
+        )
+    ])
+], className="pt-5")
+
+cards = html.Div([
+    html.Div([
+        html.Div([
+            dbc.Card([
                 dbc.CardImg(
-                    src="/static/Images/bg6.png",
+                    src="/static/images/analysis.jpg",
                     top=True,
-                    style={"opacity": 0.6},
+                    style={"opacity": 0.4},
                 ),
                 dbc.CardImgOverlay(
-                    dbc.CardBody(
-                        [
-                            html.H4("Feed All", className="title"),
-                            html.P([
-                                "A Collective dashboard for food production"
-                            ], className="card-text1"),
-                            html.P([
-                                "over the years"
-                            ], className="card-text1"),
-                        ],
-                    ),
-                ),
-            ],
-        )
-    ],
-    className="pt-5",
-)
-
-cards = html.Div(
-    [
-        html.Div(
-            [
-                html.Div(
-                    [
-                        dbc.Card(
-                            [
-                                dbc.CardImg(
-                                    src="/static/Images/bg6.png",
-                                    top=True,
-                                    style={"opacity": 0.6},
-                                ),
-                                dbc.CardImgOverlay(
-                                    dbc.CardBody(
-                                        [
-                                            html.H4(
-                                                "informative", className="card-title"
-                                            ),
-                                            html.P(
-                                                "bleh",
-                                                className="card-text",
-                                            ),
-                                        ],
-                                    ),
-                                ),
-                            ],
-                            # style={"width": "150rem"},
-                        ),
-                    ],
-                    className="col-4 mb-4",
-                ),
-                html.Div(
-                    [
-                        dbc.Card(
-                            [
-                                dbc.CardImg(
-                                    src="/static/Images/bg6.png",
-                                    top=True,
-                                    style={"opacity": 0.6},
-                                ),
-                                dbc.CardImgOverlay(
-                                    dbc.CardBody(
-                                        [
-                                            html.H4(
-                                                "informative", className="card-title"
-                                            ),
-                                            html.P(
-                                                "bleh",
-                                                className="card-text",
-                                            ),
-                                        ],
-                                    ),
-                                ),
-                            ],
-                            # style={"width": "150rem"},
-                        ),
-                    ],
-                    className="col-4 mb-4",
-                ),
-                html.Div(
-                    [
-                        dbc.Card(
-                            [
-                                dbc.CardImg(
-                                    src="/static/Images/bg6.png",
-                                    top=True,
-                                    style={"opacity": 0.6},
-                                ),
-                                dbc.CardImgOverlay(
-                                    dbc.CardBody(
-                                        [
-                                            html.H4(
-                                                "informative", className="card-title"
-                                            ),
-                                            html.P(
-                                                "bleh",
-                                                className="card-text",
-                                            ),
-                                        ],
-                                    ),
-                                ),
-                            ],                            
-                        ),
-                    ],
-                    className="col-4 mb-4",
-                ),
-            ],
-            className="row",
-        ),
+                    dbc.CardBody([
+                        html.H4([
+                            "Analysis"
+                        ], className="font-medium card-title"),
+                        html.B([
+                            "Travel through various types of graphs and interpretations"
+                        ], className="card-text"),
+                    ]),
+                )
+            ]),
+        ], className="col-4 mb-4" ),
+                
         html.Div([
-                html.Div([
-                    dbc.Card([
-                        dbc.CardImg(
-                            src="/static/Images/bg6.png",
-                            top=True,
-                            style={"opacity": 0.6},
-                        ),
-                        dbc.CardImgOverlay(
-                            dbc.CardBody([
-                                html.H4([
-                                    "informative"
-                                ], className="card-title"),
-                                html.P([
-                                    "bleh"
-                                ], className="card-text"),
-                            ]),
-                        )
+            dbc.Card([
+                dbc.CardImg(
+                    src="/static/images/info.png",
+                    top=True,
+                    style={"opacity": 0.4},
+                ),
+                dbc.CardImgOverlay(
+                    dbc.CardBody([
+                        html.H4([
+                            "Informative"
+                        ], className="card-title"),
+                        html.B([
+                            "Discover past trends and predict the ones coming"
+                        ], className="card-text"),
                     ]),
-                ], className="col-4 mb-4"),
+                )
+            ]),
+        ], className="col-4 mb-4" ),
 
-                html.Div([
-                    dbc.Card([
-                        dbc.CardImg(
-                            src="/static/Images/bg6.png",
-                            top=True,
-                            style={"opacity": 0.6},
-                        ),
-                        dbc.CardImgOverlay(
-                            dbc.CardBody([
-                                html.H4([
-                                    "informative"
-                                ], className="card-title"),
-                                html.P([
-                                    "bleh"
-                                ], className="card-text"),
-                            ]),
-                        )
+        html.Div([
+            dbc.Card([
+                dbc.CardImg(
+                    src="/static/images/access.jpg",
+                    top=True,
+                    style={"opacity": 0.4},
+                ),
+                dbc.CardImgOverlay(
+                    dbc.CardBody([
+                        html.H4([
+                            "Accessible"
+                        ], className="card-title"),
+                        html.B([
+                            "Open for all - students, public, corporations, government"
+                        ], className="card-text"),
                     ]),
-                ], className="col-4 mb-4"),
-
-                html.Div([
-                    dbc.Card([
-                        dbc.CardImg(
-                            src="/static/Images/bg6.png",
-                            top=True,
-                            style={"opacity": 0.6},
-                        ),
-                        dbc.CardImgOverlay(
-                            dbc.CardBody([
-                                html.H4([
-                                    "informative"
-                                ], className="card-title"),
-                                html.P([
-                                    "bleh"
-                                ], className="card-text"),
-                            ]),
-                        )
-                    ]),
-                    ], className="col-4 mb-4")
-
-            ], className="row"),
-    ], className="container", style={"margin-top": "5rem"})
+                )
+            ]),
+        ], className="col-4 mb-4" )
+    ], className="row"),
+], className="container", style={"margin-top": "5rem"})
 
 footer = html.Footer([
     html.Div([
@@ -264,11 +159,7 @@ btn = html.Div([
 ], className = "bttn") 
 ], className="text-center")
 
-
 app.layout = html.Div([navbar, card_img, cards, html.Br(), btn, footer], className="pt-5")
-
-# add callback for toggling the collapse on small screens
-
 
 @app.callback(
     Output("navbar-collapse", "is_open"),
@@ -279,7 +170,6 @@ def toggle_navbar_collapse(n, is_open):
     if n:
         return not is_open
     return is_open
-
 
 if __name__ == "__main__":
     app.run_server(debug=True, port=8888)
